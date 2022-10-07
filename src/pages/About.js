2b07e0d98addb381aa086d6f9cba42fccd628936
Daniel Arisa <daniel.arisa@yahoo.com>
2022-10-07 03:53:24 -0500

    Styling changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,6 +8,7 @@ const text = [
     'Welcome to WeSearch', 1000,
     'An app by Jasmin 🔥, Jeremey 🎵, and Danny 👁️ ', 1000,
     'WeSearch is a Atlanta Based search engine that puts the power back in the hands of the user', 1700,
+    'WeSearch is an app for people looking to "research" the best community driven places in their area.', 1900,
     'WeSearch takes the reviews of different establishments in the city, compiles them, and displays them for easy accessibility', 2000,
     'WeSearch also allows the user to post their own custom reviews', 1000,
     'So what are you waiting for', 800,
@@ -21,10 +22,13 @@ const text = [
 
         <div className="About Me">
             <header className="About-header"/>
-            <img src="https://danielarisa.com/wp-content/uploads/2022/10/logo-color.svg" width="1000" position="center" height="300" alt="logo" />
             <h1>About Us</h1>
-            <p><Typical wrapper="span" steps={text} loop={Infinity} className={'caca'} /></p>
-          
+            <p><Typical 
+            wrapper="span" 
+            steps={text} 
+            loop={Infinity} 
+            className={'caca'} /></p>
+          <img src="https://danielarisa.com/wp-content/uploads/2022/10/logo-color.svg" width="1000" position="bottom" height="300" alt="logo" />
         </div>
       )
     }
